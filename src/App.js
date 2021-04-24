564e88f8ec0bc8c1418d608115d6a4a6f4b78a12
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2021-04-24 18:56:56 +0100

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <body>
-        <div class="exercise-header">
-          <h2>Exercise 1 - Testimonial Block</h2>
-        </div>
+    <body>
+      <div class="exercise-header">
+        <h2>Exercise 1 - Testimonial Block</h2>
+      </div>
 
+      <div>
         <div>
           <div>
-            <div>
-              <p>
-                Gingerbread tart cupcake cake muffin cookie liquorice tiramisu.
-                Toffee cupcake cake cake croissant icing carrot cake cookie.
-                Dessert chocolate bar apple pie sesame snaps liquorice carrot
-                cake cookie danish.
-              </p>
-              <span>Indiana Jones, Archaeologist</span>
-            </div>
-            <a>Tell Me More</a>
+            <p>
+              Gingerbread tart cupcake cake muffin cookie liquorice tiramisu.
+              Toffee cupcake cake cake croissant icing carrot cake cookie.
+              Dessert chocolate bar apple pie sesame snaps liquorice carrot cake
+              cookie danish.
+            </p>
+            <span>Indiana Jones, Archaeologist</span>
           </div>
+          <a>Tell Me More</a>
         </div>
+      </div>
 
-        <div class="exercise-header">
-          <h2>Exercise 2 - Filterable Content</h2>
-        </div>
+      <div class="exercise-header">
+        <h2>Exercise 2 - Filterable Content</h2>
+      </div>
 
-        <div>[Exercise 2 Here]</div>
-      </body>
-    </div>
+      <div>[Exercise 2 Here]</div>
+    </body>
   );
 }
 
